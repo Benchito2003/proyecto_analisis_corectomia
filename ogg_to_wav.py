d1fb8716df187a0ce56527f3c87a555a31127975
chino-arch <rubenalfonsogamboa@gmail.com>
2025-12-01 18:16:22 -0600

    Normaliza masivamente todos los audios que le guardes

--- ogg_to_wav.py
+++ ogg_to_wav.py
@@ -1,9 +1,27 @@
-# Módulo para descomprimir ogg en wav
+# Módulo para descomprimir ogg en wav EN MASA
 from pydub import AudioSegment
+import glob
 
-# Cargar audio whatsapp
-audio = AudioSegment.from_file("./assets/audios/antes_cordectomia.ogg")
+# Variables de rutas
+ruta = "audios/ogg/"
+nombres_nuevos = "audio_ruidoso"
+
+# audio = AudioSegment.from_file("./assets/audios/antes_cordectomia.ogg")
+archivos = glob.glob(ruta + "*")
 
 
-# Lo exporta como audio puro (WAV)
-audio.export("audio_puro.wav", format="wav")
+def normalizar():
+    """
+    Función que convierte y normaliza automáticamente los audios
+    """
+    contador = 0
+    for i in archivos:
+        audio = AudioSegment.from_file(i)
+        audio.export(
+            "audios/wav/" + nombres_nuevos + f"{contador + 1}.wav", format="wav"
+        )
+        contador += 1
+
+
+if __name__ == "__main__":
+    normalizar()
